cd96b9b0ca6ed945ea477df291b4ef0cc6014388
Nadin <nadyawinters@gmail.com>
2021-07-02 13:14:53 +0200

    added splash animation for letters

--- src/scenes/FirstScene.js
+++ src/scenes/FirstScene.js
@@ -6,14 +6,16 @@ class FirstScene extends BaseScene {
       ...config,
     });
     this.fontSize = 2.3;
-    this.animationIsOver =false;
+    this.animationIsOver = false;
+    this.lettersAreInTheMiddle =false;
+    this.letters = [];
+    this.delay = 1000;
   }
 
   create() {
     this.createBG();
     this.navigateToMenuScene();
     this.addLogoLetters();
-    //this.animateLogo();
     super.create();
   }
 
@@ -45,7 +47,7 @@ class FirstScene extends BaseScene {
   }
 
   addLogoLetters() {
-       this.g = this.add.image(
+    this.g = this.add.image(
       this.config.width * 0.32,
       this.config.height * 0.9,
       "G"
@@ -95,74 +97,86 @@ class FirstScene extends BaseScene {
       this.config.height * 0.9,
       "S"
     );
+
+    this.letters.push(this.g, this.r, this.a1, this.p, this.h , this.l , this.a2, this.n, this.d, this.s)
+   //this.letters.push(this.r, this.p, this.a1, this.g, this.h , this.n , this.d, this.l, this.a2, this.s)
   }
 
-  moveLetter(letter,maxY, speedY){
-      if(letter.y > maxY){
-        letter.y -=speedY;
-      }
-      else{
-        letter.y = maxY;
-        this.splashAnimation(letter,letter.x, letter.y);
-      }
+  moveLetter(letter, maxY, speedY) {
+    if (letter.y > maxY) {
+      letter.y -= speedY;
+    } else {
+       letter.y = maxY;
+       this.lettersAreInTheMiddle = true;
+    
+     }
+     
   }
 
-  splashAnimation(letter,x ,y){
-    const effect = this.add.sprite(x, y, "splash", 0);
-    this.scaleObject(effect, 2);
+  splashAnimation(letter, x, y) {
+    const effect = this.add.sprite(x, y, "graySmoke", 0);
+    this.scaleObject(effect, 6);
     effect.depth = 100;
     this.anims.create({
-      key: "splashTransform",
-      frameRate: 10,
-      frames: this.anims.generateFrameNames("splash", { start: 1, end: 10 }),
+      key: "graySmokeTransform",
+      frameRate: 15,
+      frames: this.anims.generateFrameNames("graySmoke", { start: 1, end: 6 }),
     });
-    effect.play("splashTransform");
+    effect.play("graySmokeTransform");
     effect.once("animationcomplete", () => {
       effect.destroy();
       this.animationIsOver = true;
     });
-    letter.destroy();
-    this.time.delayedCall(3000,() => { this.scene.start("MenuScene");});
+    //letter.destroy();
+    //this.time.delayedCall(3000,() => { this.scene.start("MenuScene");});
   }
 
-  animateLogo(){
-  
-    this.moveLetter(this.g, this.config.height*0.46, 2)
-        //
-        //maxX = this.config.width * 0.32,
+  animateLogo() {
+    this.moveLetter(this.g, this.config.height * 0.46, 2);
+    //
+    //maxX = this.config.width * 0.32,
 
-    this.moveLetter(this.r, this.config.height*0.45,2)
-   //maxX = this.config.width * 0.36,
+    this.moveLetter(this.r, this.config.height * 0.45, 2);
+    //maxX = this.config.width * 0.36,
 
-   this.moveLetter(this.a1, this.config.height*0.47,2)
+    this.moveLetter(this.a1, this.config.height * 0.47, 2);
     //maxX = this.config.width * 0.4,
-    
-    this.moveLetter(this.p, this.config.height*0.46,2)
+
+    this.moveLetter(this.p, this.config.height * 0.46, 2);
     //maxX = this.config.width * 0.44,
 
-    this.moveLetter(this.h, this.config.height*0.465, 2)
+    this.moveLetter(this.h, this.config.height * 0.465, 2);
     //maxX =  this.config.width * 0.48,
 
-    this.moveLetter(this.l, this.config.height*0.46, 2)
+    this.moveLetter(this.l, this.config.height * 0.46, 2);
     //maxX =  this.config.width * 0.52,
 
-    this.moveLetter(this.a2, this.config.height*0.455, 2)
+    this.moveLetter(this.a2, this.config.height * 0.455, 2);
     //maxX =  this.config.width * 0.56,
 
-    this.moveLetter(this.n, this.config.height*0.45, 2)
+    this.moveLetter(this.n, this.config.height * 0.45, 2);
     //maxX =   this.config.width * 0.6,
 
-    this.moveLetter(this.d, this.config.height*0.465, 2)
+    this.moveLetter(this.d, this.config.height * 0.465, 2);
     //maxX =   this.config.width * 0.64,
 
-    this.moveLetter(this.s, this.config.height*0.46, 2)
+    this.moveLetter(this.s, this.config.height * 0.46, 2);
     //maxX =  this.config.width * 0.68,
-}
-
-  update() {
-      if(!this.animationIsOver){this.animateLogo();}
   }
 
+  update() {
+    if (!this.lettersAreInTheMiddle) {
+      this.animateLogo();
+    }else if (!this.animationIsOver){
+  
+        for (const val of this.letters){
+            this.time.delayedCall(this.delay, () => {
+            this.splashAnimation(val, val.x, val.y)});
+            this.delay += 400;
+        }
+        this.animationIsOver = true;
+    }
+  }
 }
 
 export default FirstScene;
